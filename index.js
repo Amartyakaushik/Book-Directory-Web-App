863044cafcb79a5850748c908a5963d01274f304
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-03-30 23:24:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+const Port = 5000;
+const api = require('./src/api');
+
+app.use(bodyparser.json());
+app.use('/api/v1', api);
+
+app.listen(Port, () => console.log(`Your server is running on https://${Port}`));
