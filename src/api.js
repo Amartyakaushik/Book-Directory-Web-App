c2de235d5ca391707251988d22e9eeb4d0d44e88
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-04-18 23:21:20 +0530

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,1 +1,20 @@
-abs
+const router = require('express').Router();
+
+router.get('/books', (req, res)  => {
+    res.send('Ok');
+});
+
+router.get('/books/:id', (req,res) => {
+
+});
+router.post('/books', (req,res) => {
+
+});
+router.put('/books/:id', (req,res) => {
+
+});
+router.delete('/books/:id', (req,res) => {
+
+});
+
+module.exports = router;
